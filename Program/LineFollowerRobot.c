1a3bd7998f122689f228f11f9d29ee9abadd1a20
vladubase <vladubase@gmail.com>
2020-07-19 23:33:42 +0300

    Add how to setup info in main program

--- Program/LineFollowerRobot.c
+++ Program/LineFollowerRobot.c
@@ -11,32 +11,38 @@
 	*				It has a flexible settings system via #defines at the beginning of the .C main file.
 	*				IO ports are configured through registers.
 	*	
-	*  	@attention 	"The usual procedure for setting up the PID regulator:
-	*					1. At a low speed, we adjust the P-controller (we select a value of kP such 
-	*						that in the coolest turns the robot passes keeping the line close to its
-	*						extreme sensors). kD and kI are equal to zero, i.e. use a pure P-regulator;
-	*					2. Increase the speed, select the value of kD. If the robot went without
-	*						inertia when setting the P-controller, then the kP value can be left unchanged.
-	*						If the robot has already gone with inertia, as is usually the case
-	*						with fast robots, then the kP value will need to be lowered -
-	*						we will see this by the fact that the robot will cease to deviate
-	*						strongly from the line thanks to the help of the D-controller;
-	*					3. When the PD controller is configured, then I can be selected,
-	*						reducing the deviation of the robot from the line. The values ​​of
-	*						the coefficients kD and kP are usually also somewhat lower.
-	*						An i-controller is useful for racing where a line can make loops.
-	*						Deviation of the robot from a straight line is fraught with
-	*						the choice of the wrong direction of movement. When racing on tracks
-	*						without loops, the PD controller is often used, since in the general
-	*						case it allows you to develop a higher speed."
-	*						(Credit: https://www.goodlancer.com/pid-control/).
+	*	@attention 	Before program Microcontroller check follow:
+	*			@b		F_CPU - system clock of external quartz resonator (if needed clock division, see datasheet);
+	*			@b		QTY_OF_SENSORS and used bits (see "Defines" section and "InitSys" function);
+	*			@b		MOTORS_NOT_PERFECT parameter, if motors are not identical by speed, torque, gear ratio, etc.
+	*			@b		PID coefficients
+	*
+	*  	@attention 	The usual procedure for setting up the PID regulator:
+	*			@a		1. At a low speed, we adjust the P-controller (we select a value of kP such 
+	*							that in the coolest turns the robot passes keeping the line close to its
+	*							extreme sensors). kD and kI are equal to zero, i.e. use a pure P-regulator;
+	*			@a		2. Increase the speed, select the value of kD. If the robot went without
+	*							inertia when setting the P-controller, then the kP value can be left unchanged.
+	*							If the robot has already gone with inertia, as is usually the case
+	*							with fast robots, then the kP value will need to be lowered -
+	*							we will see this by the fact that the robot will cease to deviate
+	*							strongly from the line thanks to the help of the D-controller;
+	*			@a		3. When the PD controller is configured, then I can be selected,
+	*							reducing the deviation of the robot from the line. The values ​​of
+	*							the coefficients kD and kP are usually also somewhat lower.
+	*							An i-controller is useful for racing where a line can make loops.
+	*							Deviation of the robot from a straight line is fraught with
+	*							the choice of the wrong direction of movement. When racing on tracks
+	*							without loops, the PD controller is often used, since in the general
+	*							case it allows you to develop a higher speed.
 	*
 	*   @note		4.25 ms for main cycle (not counting MAIN_CYCLE_DELAY) with 4 low-level sensors.
 	*				6 ms for main cycle (not counting MAIN_CYCLE_DELAY) with 15 low-level sensors.
 	*
 *****/
 
-/**************************** Includes ****************************/
+
+/************************************** Includes **************************************/
 
 #include <mega328p.h>
 #include <mega328p_bits.h>
@@ -45,18 +51,21 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-/***************************** Defines ****************************/
 
+/*************************************** Defines **************************************/
+
+// CLOCK
 #define		F_CPU				((uint32_t)20000000)	// Quartz resonator clock frequency
 
+// GENERAL PARAMETERS
 #define		QTY_OF_SENSORS		8U						// Quantity of sensors
 #define		AVG_SPEED			((uint8_t)155)			// Average speed of robot
 
 #define		MOTORS_NOT_PERFECT	true 					// Do the motors have different real parameters (e.g. Speed, Torque, etc.)?
 #if MOTORS_NOT_PERFECT
 	// There is nothing perfect ;)
-	#define	L_MOTOR_MISMATCH	((float)1.0)			// Coefficient of motor power difference
-	#define	R_MOTOR_MISMATCH	((float)1.07)			// Coefficient of motor power difference
+	#define	L_MOTOR_MISMATCH	((float)1.0)			// Coefficients of motor power difference
+	#define	R_MOTOR_MISMATCH	((float)1.07)
 #endif /* MOTORS_NOT_PERFECT */
 
 // PID
@@ -119,17 +128,20 @@
 
 //#define	READ_IR_SENSOR		PINx & (1 << DDxx)
 
-/************************* Global Variables ***********************/
+
+/*********************************** Global Variables *********************************/
 
 bool line_data[QTY_OF_SENSORS] = {0};					// Store current values from sensor line
 
-/*********************** Function  prototypes *********************/
+
+/********************************* Function  prototypes *******************************/
 
 void InitSys (void);
 void ReadSensorLineData (void);
 float CurrentRobotError (void);
 
-/****************************** Main ******************************/
+
+/**************************************** Main ****************************************/
 
 void main (void) {
 	// DEFINITION OF VARIABLES
@@ -212,7 +224,8 @@ void main (void) {
 	}
 }
 
-/*************************** Functions ****************************/
+
+/************************************* Functions **************************************/
 
 void InitSys (void) {
 	// Motors
