8e906db3bf5499af6b9cec720a815caffae5a303
vladubase <vladubase@gmail.com>
2020-09-26 12:04:24 +0300

    Change some //comments to be more specific.

--- DMotor/Program/LineFollowerRobot.c
+++ DMotor/Program/LineFollowerRobot.c
@@ -12,22 +12,22 @@
 	*				IO ports are configured through registers.
 	*	
 	*	@attention 	Before program Microcontroller check follow:
-	*			@b		1. F_CPU - system clock of external quartz resonator (if needed clock division, see datasheet);
-	*			@b		2. QTY_OF_SENSORS and used bits (see "Defines" section and "InitSys" function);
-	*			@b		3. MOTORS_NOT_PERFECT parameter, if motors are not identical by speed, torque, gear ratio, etc.
-	*			@b		4. PID coefficients
+	*			@a		1. F_CPU - system clock of external quartz resonator (if needed clock division, see datasheet);
+	*			@a		2. QTY_OF_SENSORS and used bits (see "Defines" section and "InitSys" function);
+	*			@a		3. MOTORS_NOT_PERFECT parameter, if motors are not identical by speed, torque, gear ratio, etc.
+	*			@a		4. PID coefficients
 	*
 	*  	@attention 	The usual procedure for setting up the PID regulator:
-	*			@a		1. At a low speed, we adjust the P-controller (we select a value of kP such 
+	*			@b		1. At a low speed, we adjust the P-controller (we select a value of kP such 
 	*							that in the coolest turns the robot passes keeping the line close to its
 	*							extreme sensors). kD and kI are equal to zero, i.e. use a pure P-regulator;
-	*			@a		2. Increase the speed, select the value of kD. If the robot went without
+	*			@b		2. Increase the speed, select the value of kD. If the robot went without
 	*							inertia when setting the P-controller, then the kP value can be left unchanged.
 	*							If the robot has already gone with inertia, as is usually the case
 	*							with fast robots, then the kP value will need to be lowered -
 	*							we will see this by the fact that the robot will cease to deviate
 	*							strongly from the line thanks to the help of the D-controller;
-	*			@a		3. When the PD controller is configured, then I can be selected,
+	*			@b		3. When the PD controller is configured, then I can be selected,
 	*							reducing the deviation of the robot from the line. The values ​​of
 	*							the coefficients kD and kP are usually also somewhat lower.
 	*							An i-controller is useful for racing where a line can make loops.
@@ -36,8 +36,8 @@
 	*							without loops, the PD controller is often used, since in the general
 	*							case it allows you to develop a higher speed.
 	*
-	*   @note		4.25 ms for main cycle (not counting MAIN_CYCLE_DELAY) with 4 low-level sensors.
-	*				6 ms for main cycle (not counting MAIN_CYCLE_DELAY) with 15 low-level sensors.
+	*   @note		4.25 ms for main cycle (not counting MAIN_CYCLE_DELAY) with 4 sensors in low-state.
+	*				6 ms for main cycle (not counting MAIN_CYCLE_DELAY) with 15 sensors in low-state.
 	*
 *****/
 
@@ -357,8 +357,8 @@ float CurrentRobotError (void) {
 
 	for (i = 0; i < QTY_OF_SENSORS; i++) {
 	    if (line_data[i] != 0) {
-            // If the data on the [i]th sensor is zero,
-            // then the sensor is located above the black line
+            // If the data on the i-th sensor is zero,
+            // then the sensor is located above the black line.
             // Odd degree to preserve the sign '-'
             current_error += pow (QTY_OF_SENSORS / 2 - 0.5 - i, 3);
         }
