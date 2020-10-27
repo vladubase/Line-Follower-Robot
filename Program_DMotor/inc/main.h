f282e6951b9a3680cd67ee933f1d3f545e96dc74
vladubase <vladubase@gmail.com>
2020-10-27 12:30:29 +0300

    Restructure code.

--- Program_DMotor/inc/main.h
+++ Program_DMotor/inc/main.h
@@ -21,7 +21,7 @@
 #include "InitSYS.h"
 #include "InitI2C.h"
 #include "ReadSensorLineData.h"
-#include "CurrentRobotError.h"
+//#include "CurrentRobotError.h"
 
 
 /*************************************** Defines **************************************/
@@ -33,16 +33,16 @@
 #define		QTY_OF_SENSORS		8U						// Quantity of sensors
 #define		AVG_SPEED			((uint8_t)	155)		// Average speed of robot
 
-#define		MOTORS_NOT_PERFECT	true 					// Do the motors have different real parameters (e.g. Speed, Torque, etc.)?
+#define		MOTORS_NOT_PERFECT	false 					// Do the motors have different real parameters (e.g. Speed, Torque, etc.)?
 #if MOTORS_NOT_PERFECT
-// There is nothing perfect ;)
-#define	L_MOTOR_MISMATCH		((float)	1.0)		// Coefficients of motor power difference
-#define	R_MOTOR_MISMATCH		((float)	1.07)
+	// There is nothing perfect ;)
+	#define	L_MOTOR_MISMATCH	((float)	1.0)		// Coefficients of motor power difference
+	#define	R_MOTOR_MISMATCH	((float)	1.07)
 #endif /* MOTORS_NOT_PERFECT */
 
 // PID
 // Setup: P -> PD -> PID
-#define		kP					((uint8_t)	1)			// Proportional	feedback coefficient
+#define		kP					((float)	0.02)		// Proportional	feedback coefficient
 #define		kI					((uint8_t)	0)			// Integral 	feedback coefficient
 #define		kD					((uint8_t)	0)			// Differential	feedback coefficient
 #define		QTY_OF_ERR			((uint8_t)	10)			// Quantity of errors in memory during last (QTY_OF_ERR * MAIN_CYCLE_DELAY) ms
