b9593dfab380c38d67419587885cc56f7e3484e1
vladubase <vladubase@gmail.com>
2021-02-01 13:39:18 +0300

    Add defines and includes.

--- Program_Linelance/Code/inc/main.h
+++ Program_Linelance/Code/inc/main.h
@@ -20,19 +20,51 @@
 #include "stdint.h"
 #include "stdbool.h"
 #include "stdio.h"
-
-// User headers
-#include "InitRCC.h"
-#include "InitSWD.h"
-#include "InitGPIO.h"
-#include "InitUART.h"
-#include "InitTIM.h"
-#include "InitADC.h"
+#include "math.h"
 
 
 /************************************** Defines ***************************************/
 
+// CLOCK
+#define		f_SYSCLK			((uint32_t)	8000000)
+#define		f_HCLK				((uint32_t)	8000000)
 
+// GENERAL PARAMETERS
+#define		QTY_OF_SENSORS		8U						// Quantity of sensors.
+#define		AVG_SPEED			((uint8_t)	155)		// Average speed of robot.
+
+#define		MOTORS_NOT_PERFECT	false 					// Do the motors have different real parameters (e.g. Speed, Torque, etc.)?
+#if MOTORS_NOT_PERFECT									// There is nothing perfect ;)
+	#define	L_MOTOR_MISMATCH	((float)	1.0)		// Coefficients of motor power difference.
+	#define	R_MOTOR_MISMATCH	((float)	1.07)
+#endif /* MOTORS_NOT_PERFECT */
+
+// PID
+// Setup: P -> PD -> PID
+#define		kP					((uint8_t)	1)			// Proportional	feedback coefficient.
+#define		kI					((uint8_t)	0)			// Integral 	feedback coefficient.
+#define		kD					((uint8_t)	0)			// Differential	feedback coefficient.
+#define		QTY_OF_ERR			((uint8_t)	10)			// Quantity of errors in memory during last (QTY_OF_ERR * MAIN_CYCLE_DELAY) ms.
+#define		MAIN_CYCLE_DELAY	((uint8_t)	2)			// The main cycle delay (in ms) for correct work of D-regulation.
+
+
+/********************************** Global variables **********************************/
+
+bool line_data[QTY_OF_SENSORS];							// Store current values from sensor line.
+
+
+/************************************** Includes **************************************/
+
+// User headers.
+#include "InitRCC.h"
+#include "InitSWD.h"
+#include "InitGPIO.h"
+#include "InitUART.h"
+//#include "InitTIM.h"
+//#include "InitADC.h"
+//#include "InitDMA.h"
+#include "func_ReadSensorLineData.h"
+#include "func_CurrentRobotError.h"
 
 
 #endif /* _MAIN_H_ */
